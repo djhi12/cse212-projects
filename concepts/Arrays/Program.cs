f72bb17fe9d10d12ba11b90d2de54f978f31ef24
djhi12 <ido20002@byui.edu>
2024-02-26 20:22:20 +0800

    updated

--- concepts/Arrays/Program.cs
+++ concepts/Arrays/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Arrays
+{
+    static void Main(string[] args)
+    {
+        // Create a dynamic array using List<T>
+        List<int> numbers = new List<int>();
+
+        // Add elements to the dynamic array
+        numbers.Add(1);
+        numbers.Add(2);
+        numbers.Add(3);
+
+        // Access and modify elements in the dynamic array
+        Console.WriteLine("Elements in the dynamic array:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+
+        // Add more elements
+        numbers.Add(4);
+        numbers.Add(5);
+
+        // Remove an element
+        numbers.Remove(2);
+
+        // Access and display the modified dynamic array
+        Console.WriteLine("Modified elements in the dynamic array:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
