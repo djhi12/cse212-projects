082f544a11a4ae9dc6834d06b78468470d00ac77
djhi12 <ido20002@byui.edu>
2024-03-01 17:44:26 +0800

    updated

--- week01/analyze/Program.cs
+++ week01/analyze/Program.cs
@@ -1,8 +1,20 @@
-﻿Console.WriteLine("\n======================\nSorting\n======================");
-Sorting.Run();
+﻿using System;
 
-Console.WriteLine("\n======================\nStandardDeviation\n======================");
-StandardDeviation.Run();
 
-Console.WriteLine("\n======================\nSearch\n======================");
-Search.Run();
+namespace Program
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\n======================\nSorting\n======================");
+            Sorting.Run();
+
+            Console.WriteLine("\n======================\nStandardDeviation\n======================");
+            StandardDeviation.Run();
+
+            Console.WriteLine("\n======================\nSearch\n======================");
+            Search.Run();
+        }
+    }
+}
