cbf9c6b2fbcf7ab064902679f60d0138c4c6d887
djhi12 <ido20002@byui.edu>
2024-03-10 17:08:10 +0800

    updated

---
+++ test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    static void Main()
+    {
+        var result = isLeapYear(1996);
+        Console.WriteLine(result);
+
+        result = isLeapYear(1900);
+        Console.WriteLine(result);
+
+        result = isLeapYear(2000);
+        Console.WriteLine(result);
+
+        result = isLeapYear(2003);
+        Console.WriteLine(result);
+
+
+        Trace.Assert(IsLeapYear(1996), "1996 should have been a leap year"); // true
+        Trace.Assert(!IsLeapYear(1900), "1900 should not have been a leap year"); // false
+        Trace.Assert(IsLeapYear(2000), "2000 should have been a leap year"); // true
+        Trace.Assert(!IsLeapYear(2003), "2003 should not have been a leap year"); // false
+    }
+
+
+}
+
